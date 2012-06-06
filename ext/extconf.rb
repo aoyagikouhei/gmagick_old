a3c9f83b81a97d4de7c4a6f95a120d703907b140
aoyagikouhei <aoyagi.kouhei@gmail.com>
2012-06-07 08:10:42 +0900

    made GraphicsMagick-config

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,3 +1,14 @@
 require "mkmf"
+
+if gmconfig = ( with_config('gm-config') || with_config('gm_config') || find_executable('GraphicsMagick-config') )
+  $stderr.puts "Using config values from %s" % [ gmconfig ]
+  $CPPFLAGS << " " + `"#{gmconfig}" --cppflags`.chomp
+  $LDFLAGS << " " + `"#{gmconfig}" --ldflags`.chomp
+  $LOCAL_LIBS << " " + `"#{gmconfig}" --libs`.chomp
+else
+  $stderr.puts "No GraphicsMagick-config... trying anyway. If building fails, please try again with",
+		" --with-gm-config=/path/to/GraphicsMagick-config"
+end
+
 create_makefile("Gmagick")
 
--- ext/gmagick_rb.c
+++ ext/gmagick_rb.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <ruby.h>
+#include <magick/api.h>
 
 VALUE test(void);
 
 Init_Gmagick(){
   VALUE module;
+  InitializeMagick(NULL);
   module = rb_define_module("Gmagick");
   rb_define_module_function(module, "test", test, 0);
 }
