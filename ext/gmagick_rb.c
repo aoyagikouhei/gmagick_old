53dde26525c842f2430a3a050f2aeef3d58376e0
aoyagikouhei <aoyagi.kouhei@gmail.com>
2012-06-07 09:21:33 +0900

    making init

--- ext/gmagick_rb.c
+++ ext/gmagick_rb.c
@@ -2,16 +2,23 @@
 #include <ruby.h>
 #include <magick/api.h>
 
-VALUE test(void);
-
-Init_Gmagick(){
-  VALUE module;
-  InitializeMagick(NULL);
-  module = rb_define_module("Gmagick");
-  rb_define_module_function(module, "test", test, 0);
-}
-
 VALUE test(void){
   printf("Hello, Ruby World.\n");
   return Qnil;
 }
+
+VALUE init(VALUE self) {
+
+  return self;
+}
+
+Init_Gmagick(){
+  VALUE mGmagick;
+  VALUE cImage;
+  InitializeMagick(NULL);
+  mGmagick = rb_define_module("Gmagick");
+  cImage = rb_define_class_under(mGmagick, "Image", rb_cObject);
+  rb_define_module_function(mGmagick, "test", test, 0);
+  rb_define_method(cImage, "initialize", init, 0);
+}
+
