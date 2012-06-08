d864474b4461f59bc6ecdb3926790f2c78863a5b
aoyagikouhei <aoyagi.kouhei@gmail.com>
2012-06-09 08:14:30 +0900

    make alloc

--- ext/gmagick_rb.c
+++ ext/gmagick_rb.c
@@ -2,6 +2,24 @@
 #include <ruby.h>
 #include <magick/api.h>
 
+void 
+gm_image_destroy(void *src)
+{
+  Image *img = (Image *)src;
+  if (NULL != img)
+  {
+    DestroyImage(img);
+  }
+}
+
+VALUE
+Image_alloc(VALUE klass)
+{
+  volatile VALUE val;
+  val = Data_Wrap_Struct(klass, NULL, gm_image_destroy, NULL);
+  return val;
+}
+
 VALUE test(void){
   printf("Hello, Ruby World.\n");
   return Qnil;
@@ -19,6 +37,7 @@ Init_Gmagick(){
   mGmagick = rb_define_module("Gmagick");
   cImage = rb_define_class_under(mGmagick, "Image", rb_cObject);
   rb_define_module_function(mGmagick, "test", test, 0);
+  rb_define_alloc_func(cImage, Image_alloc);
   rb_define_method(cImage, "initialize", init, 0);
 }
 
